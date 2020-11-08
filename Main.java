bd8486b53a379a0b362dfa5345ed0821efe52628
armend <armend.azizi@usi.ch>
2020-11-08 17:41:18 +0100

    fix #7

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args) {
+        System.out.println("Bug fixed");
         int[] array = {1,2,3,4,5,6};
         int sum = mySum(array);
         System.out.println(sum);
