e85f3f9675ed1e9c6ab049eceefd9b0def9ba97d
armend <armend.azizi@usi.ch>
2020-11-08 21:19:54 +0100

    Fixes #10 issue

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("clse issue 1");
-        System.out.println("clse issue 1");
         int[] array = {1,2,3,4,5,6};
         int sum = mySum(array);
         System.out.println(sum);
