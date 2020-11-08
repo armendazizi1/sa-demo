d6892678366ef275b35d8d4edbbeb1f8d4f774cf
armend <armend.azizi@usi.ch>
2020-11-08 17:23:05 +0100

    add sum function in Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,16 @@ public class Main{
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
+        int[] array = {1,2,3,4,5,6};
+        int sum = mySum(array);
+        System.out.println(sum);
+    }
+
+    public static int mySum(int [] array){
+        int sum = 0;
+        for(int i : array){
+            sum += i;
+        }
+        return sum;
     }
 }
