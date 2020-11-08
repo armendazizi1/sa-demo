aabc68d3bd0bb09e820ea02ee166c05718fe4f7c
armend <armend.azizi@usi.ch>
2020-11-08 21:15:16 +0100

    introduce bug 4

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main{
         int[] array = {1,2,3,4,5,6};
         int sum = mySum(array);
         System.out.println(sum);
+        System.out.println("clse issue 1");
+        System.out.println("clse issue 1");
     }
 
     public static int mySum(int [] array){
