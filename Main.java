74b180dcd533ca75e7aaa6c368fe61bd03dbe0bf
armend <armend.azizi@usi.ch>
2020-11-09 08:16:42 +0100

    Fixes bug commit A and B

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        // this fixes
+        // this
         int[] array = {1,2,3,4,5,6};
         int sum = mySum(array);
     }
