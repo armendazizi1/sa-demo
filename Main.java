07549161df53b9bfa6bbd101dc03c4d51219140f
armend <armend.azizi@usi.ch>
2020-11-21 15:58:19 +0100

    Big change

--- Main.java
+++ Main.java
@@ -1,18 +1,4 @@
 public class Main{
-    public static void main(String[] args) {
-        //
-        int[] array = {1,2,3,4,5,6};
-        int sum = mySum(array);
-
-        System.out.println("what is this??");
-    }
-
-    public static int mySum(int [] array){
-        int sum = 0;
-        for(int i=0;i <array.length; i++){
-            sum += array[i];
-        }
-        return sum;
-    }
+    public static void main(String[] args) {}
 
 }
