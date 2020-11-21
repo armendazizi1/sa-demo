761c8bccb6268fb4880aa8573e8487a06a7073e0
armend <armend.azizi@usi.ch>
2020-11-21 21:11:08 +0100

    update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,11 @@ public class Main{
     }
 
     public static int mySum(int [] array){
+
+
+
+
+        
         int sum = 0;
         for(int i=0;i <array.length; i++){
             sum += array[i];
