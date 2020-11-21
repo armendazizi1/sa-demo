b794faf5d2335b876b087009748f9f6e353301b1
Nimisha Manjali <manjan@usi.ch>
2020-11-21 19:03:51 +0100

    update check2

--- Main.java
+++ Main.java
@@ -12,6 +12,7 @@ public class Main{
         System.out.println("update check2");
         System.out.println("update check4");
         System.out.println("update check4");
+        System.out.println("update check5");
 
 
     }
