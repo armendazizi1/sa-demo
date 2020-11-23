0c1de5c3fc2230bd4f4fee97dba0e402dd2637be
armend <armend.azizi@usi.ch>
2020-11-23 16:19:14 +0100

    count commit +1

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main{
         int[] array = {1,2,3,4,5,6};
         int sum = mySum(array);
         Test test = new Test();
+        //blah
     }
 
     public static int mySum(int [] array){
