6f361f3cf5171d35aa21f6b6cc204bf7f101f34b
Susanna Ardigò <susanna.ardigo@usi.ch>
2020-11-23 19:54:17 +0100

    This programmer is so lazy made an autocommit command

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main{
     public static void main(String[] args) {
         int[] array = {1,2,3,4,5,6};
         int sum = mySum(array);
+        System.out.println(sum);
         Test test = new Test();
     }
 
