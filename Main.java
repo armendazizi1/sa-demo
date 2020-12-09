6d947565be17a74212612e4a57ca7769f8a61a8c
armend <armend.azizi@usi.ch>
2020-12-09 20:59:17 +0100

    Fixes bug #13

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ public class Main{
         int sum = mySum(array);
         System.out.println(sum);
         Test test = new Test();
-        System.out.println("This is a new bug");
     }
 
     public static int mySum(int [] array){
